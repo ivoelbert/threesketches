60cfacb9088abd2c31e594dd75c31fbfd4f5f68c
Ivo Elbert <ivogamebot@gmail.com>
2018-03-01 12:07:54 -0300

    setup.sh now cleans hs/ directory

--- setup.sh
+++ setup.sh
@@ -2,4 +2,9 @@ echo "Setting up..."
 cd src/resources/hs
 ./scene
 cp update.js ../js/update.js
+echo "Cleaning up..."
+rm *.o
+rm *.hi
+rm *.jsmod
+rm update.js
 echo "ENDED - Please check for errors above."
